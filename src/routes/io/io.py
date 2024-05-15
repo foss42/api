8d3a0aab9ab14ba011015d43fb2d1b6312210c51
Harsh Panchal <harsh.panchal.0910@gmail.com>
2024-05-15 16:10:19 +0530

    new put api added

--- src/routes/io/io.py
+++ src/routes/io/io.py
@@ -60,7 +60,7 @@ async def create_file(request: Request):
                        "deduced-mime-type": deduced_type})
     except:
         raise internal_error_500()
-
+file_data = {}
 
 @io_router.post('/img')
 async def analyze_img_file(
@@ -72,11 +72,54 @@ async def analyze_img_file(
                                  2)
         magic_conf = puremagic.magic_stream(imfile.file)
         deduced_type = magic_conf[0].mime_type if len(magic_conf) > 0 else None
+        file_data[imfile.filename] = {
+            "provided-token": token,
+            "size": size,
+            "content-type": imfile.content_type,
+            "file-name": imfile.filename,
+            "deduced-mime-type": deduced_type
+        }
+        print(file_data)
         return ok_200({
             "provided-token": token,
             "size": size,
             "content-type": imfile.content_type,
             "file-name": imfile.filename,
             "deduced-mime-type": deduced_type})
+    except:
+        raise internal_error_500()
+
+print(file_data)
+@io_router.put('/img')
+async def analyze_img_file_put(
+    new_imfile: Annotated[UploadFile, File()],
+    new_token: Annotated[str, Form()],
+    old_imfile: Annotated[UploadFile, File()] ,
+):
+    try:
+        size2 = hz.humanize_bytes(new_imfile.size, 2)
+        magic_conf2 = puremagic.magic_stream(new_imfile.file)
+        deduced_type2 = magic_conf2[0].mime_type if len(magic_conf2) > 0 else None
+        old_file_data = file_data[old_imfile.filename]
+        new_file_data= {
+            "provided-token": new_token,
+            "size": size2,
+            "content-type": new_imfile.content_type,
+            "file-name": new_imfile.filename,
+            "deduced-mime-type": deduced_type2
+        }
+        file_data[new_imfile.filename] = new_file_data
+        return ok_200({
+            "old-token": old_file_data["provided-token"] ,
+            "provided-token": new_token,
+            "old_size": old_file_data["size"],
+            "new_size": size2,
+            "old_content-type": old_file_data["content-type"],
+            "new_content-type": new_imfile.content_type,
+            "old_file-name": old_file_data["file-name"],
+            "new_file-name": new_imfile.filename,
+            "old_deduced-mime-type": old_file_data["deduced-mime-type"],
+            "new_deduced-mime-type": deduced_type2
+        })  
     except:
         raise internal_error_500()
